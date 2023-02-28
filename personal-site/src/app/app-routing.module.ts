7a830ecfe4a5a2a7b3d91682c8a760aaa97af642
Morgan West <mrw6863@rit.edu>
2023-02-28 14:38:28 -0500

    feat: :sparkles: remove initial template code
    removed inital template code, began configuring what the site should look like. includes a resume page

--- personal-site/src/app/app-routing.module.ts
+++ personal-site/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'resume', component: ResumeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
