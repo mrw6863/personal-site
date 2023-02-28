6c439e98b77d20b21cf34cb85f2990b0f3298bda
Morgan West <mrw6863@rit.edu>
2023-02-28 15:33:09 -0500

    feat: :sparkles: add new cards
    add new cards: projects, reading, and writing. Created corresponsing components

--- personal-site/src/app/app-routing.module.ts
+++ personal-site/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReadingComponent } from './reading/reading.component';
 import { ResumeComponent } from './resume/resume.component';
+import { WritingComponent } from './writing/writing.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'resume', component: ResumeComponent }
+  { path: 'resume', component: ResumeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'reading', component: ReadingComponent },
+  { path: 'writing', component: WritingComponent }
 ];
 
 @NgModule({
---
+++ personal-site/src/app/reading/reading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reading',
+  templateUrl: './reading.component.html',
+  styleUrls: ['./reading.component.scss']
+})
+export class ReadingComponent {
+  title = 'reading';
+}
